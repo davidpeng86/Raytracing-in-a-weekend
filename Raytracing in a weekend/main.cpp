f3407d9e090411e02d5d702e24032218f1e35f2d
Dubito-mac <peng86david@gmail.com>
2019-11-17 12:21:51 +0800

    Initial Commit

---
+++ Raytracing in a weekend/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracing in a weekend
+//
+//  Created by Dubito on 2019/11/17.
+//  Copyright © 2019 Dubito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
