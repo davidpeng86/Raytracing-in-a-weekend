0c5c72583941e125447b41af3f02e5617e5d65ab
Dubito-mac <peng86david@gmail.com>
2019-11-17 12:31:44 +0800

    Chapter 2 :
    build vec3 class header

--- Raytracing in a weekend/main.cpp
+++ Raytracing in a weekend/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "vec3.hpp"
 
 using namespace std;
 
@@ -16,15 +17,11 @@ int main(int argc, const char * argv[]) {
     cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--){
         for (int i = 0; i < nx; i++){
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            
-            // %3i
-            int ir = int (255.99*r);
-            int ig = int (255.99*g);
-            int ib = int (255.99*b);
-            
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int (255.99*col[0]);
+            int ig = int (255.99*col[1]);
+            int ib = int (255.99*col[2]);
+             
             //cout << ir << " " << ig << " " << ib << "   ";
             printf("%3i %3i %3i ", ir, ig, ib);
         }
